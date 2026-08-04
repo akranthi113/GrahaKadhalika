b8c334db85d30c62e34e51e978fcdfd0ff0509bf
akranthi113 <akranthi113@gmail.com>
2026-08-04 12:01:11 +0530

    Show logo next to GrahaKadhalika in the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -34,6 +34,7 @@ export default function Navbar() {
     <nav className="navbar">
       <div className="navbar-container">
         <Link to="/" className="navbar-brand" onClick={closeMenu}>
+          <img src="/logo.png" alt="GrahaKadhalika logo" className="navbar-logo" width="32" height="34" />
           GrahaKadhalika
         </Link>
         <button
