f5beb31cfb4db919111075890f6fb964c6bc24fb
akranthi113 <akranthi113@gmail.com>
2026-08-05 10:41:22 +0530

    Add PWA support with vite-plugin-pwa service worker

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: false,
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,svg,png,ico,woff2,wasm,zip}'],
+        globIgnores: ['ephemeris/**', '**/ephemeris/*.se1', 'ephemeris/sat/**'],
+        navigateFallback: '/index.html',
+      },
+      includeAssets: [],
+    }),
+  ],
   base: '/',
 })
